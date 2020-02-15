ad7d085c010ba0849a7668d2dce39a909f237188
Tsagan Garyaeva <simplewebby@outlook.co>
2020-02-15 13:26:26 -0500

    more changes

---
+++ ppp_team1/src/components/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+
+
+function Home() {
+  return (
+    <div className="Home">
+      <h1>Welcome to Pen Pals Platform</h1>
+      
+    </div>
+  );
+}
+
+export default Home;
